d093e7eca8137b325ee52e158568d30a85424d08
somplex1 <168852106+somplex1@users.noreply.github.com>
2025-05-20 04:47:21 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -61,5 +61,6 @@ def send_daily():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render sets the PORT env var
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
